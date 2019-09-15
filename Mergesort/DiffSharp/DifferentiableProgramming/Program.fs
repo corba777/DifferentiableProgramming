d5fa61ed79aff255d446aabcfaf96d12e48f057f
Artem Zvyagintsev <artem.zvyagintsev@gmail.com>
2019-09-15 17:03:08 -0400

    diffsharp changes

--- Mergesort/DiffSharp/DifferentiableProgramming/Program.fs
+++ Mergesort/DiffSharp/DifferentiableProgramming/Program.fs
@@ -4,34 +4,40 @@
 open FSharp
 open DiffSharp.AD
 open DiffSharp.AD.Float32
+open DiffSharp.AD.Float32
 
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv    
-    let zero=D.Zero
-    let f (x :D)= 
-        if(x= D 0.0f)
-        then -x
-        else x    
+    let split (arr : DV) =
+        let n = arr.Length
+        arr.[0..n/2-1], arr.[n/2..n-1]
+ 
+    let rec merge (l : DV) (r : DV) =
+        let n = l.Length + r.Length
+        let res = Array.zeroCreate<'a> n
+        let mutable i, j = 0, 0
+        for k = 0 to n-1 do
+            if i >= l.Length   then res.[k] <- r.[j]; j <- j + 1
+            elif j >= r.Length then res.[k] <- l.[i]; i <- i + 1
+            elif l.[i] < r.[j] then res.[k] <- l.[i]; i <- i + 1
+            else res.[k] <- r.[j]; j <- j + 1
 
-    let df=diff f
-    
-     
-
-    let dfv= df(D 0.0f)           
-    
-
-    let df0=df(D 0.0f)
-    
-    
-
-    //------------------------------------------------------------------------------------------------------------//
-    
+        toDV res
+        
+    let rec mergeSort (arr :DV) =
+       if arr.Length<2 then arr else       
+       let (x, y) = split arr
+       merge (mergeSort x) (mergeSort y)
    
+    let test =DV [|5.0f; 4.0f;3.0f;2.0f;1.0f|]
+    let res=mergeSort(test)
+
+    let jacobian_mergesort=jacobian mergeSort
+    let dres=jacobian_mergesort test 
+    let matr= dres.ToString()
 
 
-
-
+    printfn "%s" matr 
 
     0 // return an integer exit code
---
+++ Mergesort/DiffSharp/DifferentiableProgramming/mergesort_with_dv.fsx
@@ -0,0 +1,33 @@
+﻿#r "../packages/DiffSharp.0.7.7/lib/net46/DiffSharp.dll"
+
+open FSharp
+open DiffSharp.AD
+open DiffSharp.AD.Float32
+
+
+let split (arr : DV) =
+    let n = arr.Length
+    arr.[0..n/2-1], arr.[n/2..n-1]
+ 
+let rec merge (l : DV) (r : DV) =
+    let n = l.Length + r.Length
+    let res = Array.zeroCreate<D> n
+    let mutable i, j = 0, 0
+    for k = 0 to n-1 do
+        if i >= l.Length   then res.[k] <- r.[j]; j <- j + 1
+        elif j >= r.Length then res.[k] <- l.[i]; i <- i + 1
+        elif l.[i] < r.[j] then res.[k] <- l.[i]; i <- i + 1
+        else res.[k] <- r.[j]; j <- j + 1
+    toDV res
+        
+let rec mergeSort (arr :DV) =
+    if arr.Length<2 then arr else       
+    let (x, y) = split arr
+    merge (mergeSort x) (mergeSort y)
+   
+let test =DV [|5.0f; 4.0f;3.0f;2.0f;1.0f|]
+let res=mergeSort(test)
+
+let jacobianmergesort=jacobian mergeSort
+let rr=jacobianmergesort test
+rr
